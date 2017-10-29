5dc6e603774cdaab8891daf194945c08de25aba4
Sergey Goliney <sergey@goliney.com>
2017-10-30 00:40:24 +0200

    Add endpoints.json with urls

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { version } from "../package.json";
+import ENDPOINTS from "./endpoints.json";
 
 export default class Qrize {
   constructor() {
     this.version = version;
+    this.ENDPOINTS = ENDPOINTS;
   }
 }
