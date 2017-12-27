96eebc93754821ce9f983e61f92f9b7e2dc8b7e7
Sergey Goliney <sergey@goliney.com>
2017-12-27 22:45:26 +0200

    Release 0.3.0

--- dist/qrize.cjs.js
+++ dist/qrize.cjs.js
@@ -4,7 +4,7 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 
 var qrcode = _interopDefault(require('qrcode-generator'));
 
-var version = "0.2.1";
+var version = "0.3.0";
 
 function prepareCallback(callback) {
   return typeof callback === "function" ? callback : function () {};
